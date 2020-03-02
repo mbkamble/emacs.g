e977ca212f0f827d00141efbd3436aafc9f93520
Milind Kamble <Milind.Kamble@amd.com>
2020-03-02 14:36:05 -0600

    Assimilate winum v2.1.0-5-gc5455e8

--- init.el
+++ init.el
@@ -148,6 +148,37 @@
   (add-to-list 'tramp-default-proxies-alist
                (list (regexp-quote (system-name)) nil nil)))
 
+(use-package winum
+    :init
+    (setq winum-keymap
+          (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-`") 'winum-select-window-by-number)
+            (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+            (define-key map (kbd "M-1") 'winum-select-window-1)
+            (define-key map (kbd "M-2") 'winum-select-window-2)
+            (define-key map (kbd "M-3") 'winum-select-window-3)
+            (define-key map (kbd "M-4") 'winum-select-window-4)
+            (define-key map (kbd "M-5") 'winum-select-window-5)
+            (define-key map (kbd "M-6") 'winum-select-window-6)
+            (define-key map (kbd "M-7") 'winum-select-window-7)
+            (define-key map (kbd "M-8") 'winum-select-window-8)
+            map))
+    :config
+    (defun winum-assign-9-to-calculator-8-to-flycheck-errors ()
+      (cond
+       ((equal (buffer-name) "*Calculator*") 9)
+       ((equal (buffer-name) "*Flycheck errors*") 8)))
+
+    (defun winum-assign-0-to-neotree ()
+      (when (string-match-p (buffer-name) ".*\\*NeoTree\\*.*") 10))
+
+    (add-to-list 'winum-assign-functions #'winum-assign-9-to-calculator-8-to-flycheck-errors)
+    ;;(add-to-list 'winum-assign-functions #'winum-assign-0-to-neotree)
+    (setq winum-ignored-buffers '(" *which-key*")
+          winum-auto-setup-mode-line nil)
+    (winum-mode)
+    )
+
 (progn ;     startup
   (message "Loading %s...done (%.3fs)" user-init-file
            (float-time (time-subtract (current-time)
